20a688c053a5cfa526885c8f31852b576ed18c39
Sonya Borisova <sonchas3305@gmail.com>
2025-04-01 16:55:52 +0300

    -

--- Task5/File/src/main/java/FileAnalyzer.java
+++ Task5/File/src/main/java/FileAnalyzer.java
@@ -1,21 +1,47 @@
+import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths; // путь
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+
+interface Path {
+    List<String> readFile(String path) throws IOException;
+}
+
+interface Report {
+    void generateReport(Map<String, Integer> words);
+}
+
+class ConsolePath implements Path {
+    @Override
+    public List<String> readFile(String path) throws IOException {
+        return Files.readAllLines(Paths.get(path));
+    }
+}
+
+class ConsoleReport implements Report {
+    @Override
+    public void generateReport(Map<String, Integer> words) {
+        System.out.println("Частота слов в файле:");
+        for (Map.Entry<String, Integer> entry : words.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
 
 public class FileAnalyzer {
+    private final Path path;
+    private final Report report;
 
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+    public FileAnalyzer(Path path, Report report) {
+        this.path = path;
+        this.report = report;
+    }
 
-        System.out.print("Введите путь к файлу: ");
-        String filePath = scanner.nextLine();
-
+    public void analyze(String filePath) throws IOException {
         Map<String, Integer> wordCount = new HashMap<>();
-
-        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        List<String> lines = path.readFile(filePath);
 
         for (String line : lines) {
             line = line.replaceAll("[^a-zA-Zа-яА-Я0-9 ]+", "").toLowerCase();
@@ -27,13 +53,24 @@ public class FileAnalyzer {
                 }
             }
         }
+        report.generateReport(wordCount);
+    }
 
-        System.out.println("Частота слов в файле:");
-        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
-            System.out.println(entry.getKey() + ": " + entry.getValue());
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Укажите путь к файлу.");
+            return;
         }
 
-        scanner.close();
+        String path = args[0];
+        FileAnalyzer analyzer = new FileAnalyzer(new ConsolePath(), new ConsoleReport());
+
+        try {
+            analyzer.analyze(path);
+        } catch (IOException e) {
+            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        }
     }
 }
+
 // C:\Users\User\Desktop\Сбертех\File.txt
