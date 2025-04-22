1acbf510723a6862ec5dfaab9e0ff35582ce850f
Sonya Borisova <sonchas3305@gmail.com>
2025-04-22 11:13:45 +0300

    -

--- Task7/Shell/src/main/java/App.java
+++ Task7/Shell/src/main/java/App.java
@@ -23,17 +23,24 @@ public class App {
         HelpCommand helpCommand = new HelpCommand(shell.getCommandMap());
         shell.registerCommand(helpCommand);
 
+        shell.registerCommand(new DateCommand());
+        shell.registerCommand(new TimeCommand());
+        shell.registerCommand(new PwdCommand());
+        shell.registerCommand(new ExitCommand(shell));
+
         Set<Class<?>> commandClasses = analyzer.findCommandClasses(Shell.class.getPackage().getName());
         for (Class<?> clazz : commandClasses) {
             try {
-                if (clazz != HelpCommand.class) {
+                if (!HelpCommand.class.isAssignableFrom(clazz)) {
                     Command command;
-                    if (clazz == ExitCommand.class) {
+                    if (ExitCommand.class.isAssignableFrom(clazz)) {
                         command = (Command) clazz.getDeclaredConstructor(Shell.class).newInstance(shell);
                     } else {
                         command = (Command) clazz.getDeclaredConstructor().newInstance();
                     }
-                    shell.registerCommand(command);
+                    if (!shell.getCommandMap().containsKey(command.getName())) {
+                        shell.registerCommand(command);
+                    }
                 }
             } catch (Exception e) {
                 System.err.println("Ошибка при создании команды " + clazz.getSimpleName() + ": " + e.getMessage());
